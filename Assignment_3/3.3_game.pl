24fd59b4174880b54908b4534a9109aeb5c53082
豪 付 <fuhao_dsst@163.com>
2020-12-04 18:18:03 +0800

    3.1: upload 3.3: write game for two computer players

--- Assignment_3/3.3_game.pl
+++ Assignment_3/3.3_game.pl
@@ -1,7 +1,16 @@
-% calculates the utility value
+
+% -----------------------------------------------------------
+% get_utility_value calculates the utility value of a decision.
 % get_utility_value(N,Player,Value)
 % Player Max: 1, Player Min: -1
-% Value has only two possibility: 1(Max wins), -1(Min wins) 
+% returns Value, which has only two possibility: 1(Max wins), -1(Min wins) 
+% -----------------------------------------------------------
+%   when the utility values with tree actions are the same, returns this value.
+get_utility_value_result(1,1,1,_,1).
+get_utility_value_result(-1,-1,-1,_,-1).
+%   when the three utility values are not same, the results depends on the Player, so returns 1 when Max, -1 when Min.
+get_utility_value_result(N_1,N_2,N_3,Player,Value):-(N_1+N_2+N_3)<3,(N_1+N_2+N_3)>(-3),Value=Player.
+
 
 % when N is 1,2 or 3, the Player, who is acting, wins.
 get_utility_value(1,Player,Player).
@@ -9,16 +18,10 @@ get_utility_value(2,Player,Player).
 get_utility_value(3,Player,Player).
 
 % when N > 3
-%   when the utility values with tree actions are the same, returns this value.
-get_utility_value(1,1,1,_,1).
-get_utility_value(-1,-1,-1,_,-1).
-%   when the three utility values are not same, the results depends on the Player, so returns 1 when Max, -1 when Min.
-get_utility_value((N_1,N_2,N_3,Player,Value):- (N_1+N_2+N_3)<3,(N_1+N_2+N_3)>-3,Value=Player.
-
 get_utility_value(N,Player,Value):-
     N>3,
 
-        Player_next=Player*(-1),
+        Player_next is Player*(-1),
 
         N_1 is N-1,
             get_utility_value(N_1,Player_next,Value_1),
@@ -27,5 +30,115 @@ get_utility_value(N,Player,Value):-
         N_3 is N-3,
             get_utility_value(N_3,Player_next,Value_3),
         
-        get_utility_value(N_1,N_2,N_3,Player,Value).
-        
+        get_utility_value_result(Value_1,Value_2,Value_3,Player,Value).
+
+
+% -----------------------------------------------------------
+% decides how many matchs are taken away
+% decision(Player,N,Action)
+% returns 1,2 or 3 as Action
+% -----------------------------------------------------------
+
+% when the Max can find a Max or the Min can find a Min, returns that Action.
+% when can not randomly decides one.
+make_decision(Player,Player,_,_,1).
+make_decision(Player,_,Player,_,2).
+make_decision(Player,_,_,Player,3).
+make_decision(Player,Value_1,Value_2,Value_3,Action):-
+    not(Player=Value_1),
+    not(Player=Value_2),
+    not(Player=Value_3),
+    random(1,4,Action).
+
+% when remaining matchs =< 3, the Player will take all the remains to win the game.
+decision(_,1,1).
+decision(_,2,2).
+decision(_,3,3).
+
+% when remaining matchs >3, first calculates the utility value, and make decision.
+decision(Player,N,Action):-
+
+    Player_next is Player*(-1),
+
+    N_1 is N-1,
+        get_utility_value(N_1,Player_next,Value_1),
+    N_2 is N-2,
+        get_utility_value(N_2,Player_next,Value_2),
+    N_3 is N-3,
+        get_utility_value(N_3,Player_next,Value_3),
+
+    make_decision(Player,Value_1,Value_2,Value_3,Action).
+
+% -----------------------------------------------------------
+% prints the current state, action and state_dot
+% print(Player,Action,Remains)
+% -----------------------------------------------------------
+
+print(1,Action,0):- write("Max take "),write(Action),write(" matchs, and win the game."),nl.
+print(-1,Action,0):-write("Min take "),write(Action),write(" matchs, and win the game."),nl.
+print(Player,Action,Remains):-
+    Remains>0,
+        (Player=1,write("Max take ");Player=(-1),write("Min take ")),
+
+        write(Action),write(" matchs, and "),
+        write(Remains),write(" remain"),
+        nl.
+
+% -----------------------------------------------------------
+% runs the game with Max and Min
+% game(N)
+% N is the number of matchs at the begining.
+% -----------------------------------------------------------
+
+% state(Round,N,Player)
+
+% initiates the game
+init_game(N):-
+
+    asserta(state(1,N,1)).
+
+% runs the game
+
+run_game:-
+
+    state(Round,N,Player),
+    retract(state(Round,N,Player)),
+    write("Round "),write(Round),write(": "),
+
+    decision(Player,N,Action),
+
+    Remains is N-Action,
+    print(Player,Action,Remains),
+
+    (Remains>0,
+    New_round is Round+1,
+    New_player is Player*(-1),
+    assertz(state(New_round,Remains,New_player)),
+    run_game;
+
+    Remains=<0).
+
+game(N):-
+
+    init_game(N),
+
+    run_game.
+    
+% -----------------------------------------------------------
+% runs the game with Max and a human player.
+% game_Max_human(N)
+% N is the number of matchs at the begining.
+% -----------------------------------------------------------
+
+decision_human(Action):-
+    write("I want to take: "),
+    read(Action).
+
+init_game_human(Player):
+
+    write("I want to be (Max/Min)"),nl,
+    read(Action).
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % to be continue
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
